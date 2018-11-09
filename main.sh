c1ccaaddce9ee16f8c78250cb5441ac2886eff28
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-11-09 16:50:10 +0100

    separate run.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 set -e
 
-source settings.sh
-source lib.sh
-
-function slow_run {
-	echo -n " > "
-	slow_write "$@"
-	"$@"
-}
-
-cd tanks
-
 clear
 
 while true
 do
-slow_run make
-slow_run ./main "$@" SimpleBot SimpleBot
+./run.sh "$@"
 done
